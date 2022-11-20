c9a3c6ba22402505775c06d950033b473eaacccf
Hoàng Minh Huệ <httt2hauik15@gmail.com>
2022-11-20 12:14:25 +0700

    on tap sql server

--- giai.sql
+++ giai.sql
@@ -174,3 +174,207 @@ where ShippedDate-RequiredDate>7
 order by EmployeeID
 
 --ex23
+select CompanyName as 'CompanyName', Phone as 'Phone'  from Customers
+where CompanyName like 'W%'
+Union
+select (LastName + ' '+FirstName) as 'CompanyName', HomePhone 'Phone' from Employees
+order by CompanyName desc
+
+--ex24
+select orders.CustomerID, CompanyName, ContactName, ContactTitle
+from Customers
+inner join Orders on Orders.CustomerID=Customers.CustomerID
+where OrderID=10643
+
+--ex25
+select Products.ProductID,ProductName, sum(Quantity) as [Total Ordered] from products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID,ProductName
+having  sum(Quantity)>1200
+
+--ex26
+select Products.ProductID,ProductName, SupplierID,CategoryID,sum(Quantity) as [Total Ordered] from products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID,ProductName,SupplierID,CategoryID
+having  sum(Quantity)>1400
+order by ProductName
+
+--ex27
+select products.CategoryID, CategoryName,count(products.CategoryID) 'Total products'
+from Products
+inner join Categories on Categories.CategoryID=Products.CategoryID
+group by CategoryName,products.CategoryID
+having count(products.CategoryID)=(select TOP 1  count(products.CategoryID) as 'Total products'
+from Products
+inner join Categories on Categories.CategoryID=Products.CategoryID
+group by CategoryName,products.CategoryID
+order by count(products.CategoryID) desc)
+
+--ex28
+select products.CategoryID, CategoryName,count(products.CategoryID) 'Total products'
+from Products
+inner join Categories on Categories.CategoryID=Products.CategoryID
+group by CategoryName,products.CategoryID
+having count(products.CategoryID)=(select TOP 1  count(products.CategoryID) as 'Total products'
+from Products
+inner join Categories on Categories.CategoryID=Products.CategoryID
+group by CategoryName,products.CategoryID
+order by count(products.CategoryID) )
+
+--ex29
+declare @customers int;
+declare @employees int;
+declare @total int;
+set @customers=(select Count(*)  from Customers)
+set @employees=(select Count(*) from Employees)
+set @total=@customers+@employees
+select @total 'Total record'
+
+--ex 30
+select orders.EmployeeID, LastName,FirstName,Title, count(orders.EmployeeID) 'total orders'
+from Employees
+inner join Orders on Orders.EmployeeID=Employees.EmployeeID
+group by  orders.EmployeeID, LastName,FirstName,Title
+having count(orders.EmployeeID)=(select top 1 count(orders.EmployeeID) 
+from Employees
+inner join Orders on Orders.EmployeeID=Employees.EmployeeID
+group by  orders.EmployeeID
+order by count(orders.EmployeeID) 
+)
+
+--ex 31
+select orders.EmployeeID, LastName,FirstName,Title, count(orders.EmployeeID) 'total orders'
+from Employees
+inner join Orders on Orders.EmployeeID=Employees.EmployeeID
+group by  orders.EmployeeID, LastName,FirstName,Title
+having count(orders.EmployeeID)=(select top 1 count(orders.EmployeeID) 
+from Employees
+inner join Orders on Orders.EmployeeID=Employees.EmployeeID
+group by  orders.EmployeeID
+order by count(orders.EmployeeID) desc
+)
+
+--ex32
+select ProductID, ProductName, SupplierID, CategoryID, UnitsInStock
+from Products
+where UnitsInStock=(select max(UnitsInStock) from Products)
+
+--ex 33
+select ProductID, ProductName, SupplierID, CategoryID, UnitsInStock
+from Products
+where UnitsInStock=(select min(UnitsInStock) from Products)
+
+--ex 34
+select ProductID, ProductName, SupplierID, CategoryID, UnitsOnOrder
+from Products
+where UnitsOnOrder=(select max(UnitsOnOrder) from Products)
+
+--ex 35
+select ProductID, ProductName, SupplierID, CategoryID, ReorderLevel
+from Products
+where ReorderLevel=(select max(ReorderLevel) from Products)
+
+--ex 36
+select Employees.EmployeeID, LastName,FirstName, count(Employees.EmployeeID) 'Delay Orders'
+from Orders
+inner join Employees on Employees.EmployeeID=Orders.EmployeeID
+where ShippedDate-RequiredDate>0
+group by Employees.EmployeeID, LastName,FirstName
+having count(Employees.EmployeeID)=(select top 1  count(Employees.EmployeeID) 'Delay Orders'
+from Orders
+inner join Employees on Employees.EmployeeID=Orders.EmployeeID
+where ShippedDate-RequiredDate>0
+group by Employees.EmployeeID
+order by count(Employees.EmployeeID) desc)
+
+
+--ex 37
+select Employees.EmployeeID, LastName,FirstName, count(Employees.EmployeeID) 'Delay Orders'
+from Orders
+inner join Employees on Employees.EmployeeID=Orders.EmployeeID
+where ShippedDate-RequiredDate>0
+group by Employees.EmployeeID, LastName,FirstName
+having count(Employees.EmployeeID)=(select top 1  count(Employees.EmployeeID) 'Delay Orders'
+from Orders
+inner join Employees on Employees.EmployeeID=Orders.EmployeeID
+where ShippedDate-RequiredDate>0
+group by Employees.EmployeeID
+order by count(Employees.EmployeeID))
+
+
+--ex 38
+declare @top2 int;
+set @top2=(select top 1 sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID
+having 
+sum(Quantity)<(select  top 1 sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID order by sum(quantity) desc)
+order by sum(quantity) desc)
+
+declare @top3 int;
+set @top3=(select top 1 sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID
+having sum(Quantity)<@top2 order by  sum(quantity) desc)
+
+select  top 3  products.ProductID,ProductName, sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID,ProductName
+having sum(Quantity)>=@top3
+order by sum(quantity) asc
+
+--ex 39
+declare @top2 int;
+set @top2=(select top 1 sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID
+having 
+sum(Quantity)<(select  top 1 sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID order by sum(quantity) desc)
+order by sum(quantity) desc)
+
+declare @top3 int;
+set @top3=(select top 1 sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID
+having sum(Quantity)<@top2 order by  sum(quantity) desc)
+
+declare @top4 int;
+set @top4=(select top 1 sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID
+having sum(Quantity)<@top3 order by  sum(quantity) desc)
+
+declare @top5 int;
+set @top5=(select top 1 sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID
+having sum(Quantity)<@top4 order by  sum(quantity) desc)
+
+select  top 5  products.ProductID,ProductName, sum(Quantity) 'Total Ordered'from Products
+inner join [Order Details] on [Order Details].ProductID=Products.ProductID
+group by Products.ProductID,ProductName
+having sum(Quantity)>=@top5
+order by sum(quantity) asc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
